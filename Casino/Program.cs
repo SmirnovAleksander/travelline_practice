8665c3f515b32548697f2f58befd99141e66cfb8
Alexander Smirnov <aleksander50.500@gmail.com>
2024-04-20 11:23:18 +0300

    разбил по функциям и исправил замечания

--- Casino/Program.cs
+++ Casino/Program.cs
@@ -1,15 +1,49 @@
-﻿int balance = 10000;
-Random random = new Random();
-while (balance > 0)
+﻿
+internal static class Program
 {
-    Console.Write("Введите ставку: ");
-    var BetStr = Console.ReadLine();
-    int bet = int.Parse(BetStr);
+    private static int balance = 10000;
+    private static Random random = new Random();
 
-    if (bet <= balance)
+    public static void Main()
+    {
+        while (balance > 0)
+        {
+            int bet = GetValueFromUser();
+            if (bet <= balance)
+            {
+                int randomNum = random.Next(1, 20);
+                UpdateBalance(bet, randomNum);
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно денег для ставки");
+            }
+        }
+        Console.WriteLine("Вы проиграли");
+    }
+
+    private static int GetValueFromUser()
+    {
+        int bet;
+        while (true)
+        {
+            Console.Write("Введите ставку: ");
+            string betStr = Console.ReadLine();
+            if(int.TryParse(betStr, out bet))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Вы не ввели число. Введите число");
+            }
+        }
+        return bet;
+    }
+
+    private static void UpdateBalance(int bet, int randomNum)
     {
         Console.WriteLine("Баланс: " + balance);
-        int randomNum = random.Next(1, 20);
         if (randomNum == 18 || randomNum == 19 || randomNum == 20)
         {
             balance += bet * (1 + (1 * randomNum % 17));
@@ -19,9 +53,4 @@ while (balance > 0)
             balance -= bet;
         }
     }
-    else
-    {
-        Console.WriteLine("Недостаточно денег для ставки");
-    }
 }
-Console.WriteLine("Вы проиграли");
