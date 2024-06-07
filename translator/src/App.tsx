55f8ee9de8dcec5b79f6dbef756d4a3d2401b84d
Alexander Smirnov <aleksander50.500@gmail.com>
2024-06-07 22:30:30 +0300

    init commit

--- translator/src/App.tsx
+++ translator/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Button, Input} from "@mui/material";
+
 
 function App() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div>
+      <Button>Hello</Button>
+    </div>
   );
 }
 
